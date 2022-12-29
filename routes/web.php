239db3b5df88a2a58ad3330a1e5a025fbf27bc95
“derrick” <derrickmbugua50@gmail.com>
2022-12-29 15:25:22 +0300

    feat: create and edit tasks

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('task', TaskController::class);
+Route::post('Custom-sortable', [TaskController::class, "updatePosition"]);
+Route::get('/new-task', [TaskController::class, "new"])->name('new-task');
+Route::post('/update-task/{id}', [TaskController::class, "updateTask"])->name("update-task");
